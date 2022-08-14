9ebb5c11d466a4ec649c718406ffbbbbe336ca09
iLearner <me@ilearner.dev>
2022-08-14 18:32:28 +0200

    feat: set camera properly on the character

--- src/scene/Camera.ts
+++ src/scene/Camera.ts
@@ -14,12 +14,17 @@ export default class Camera {
 
   setInstance(scene: THREE.Scene, sizes: Sizes) {
     this.instance = new THREE.PerspectiveCamera(
-      35,
+      25,
       sizes.width / sizes.height,
       0.1,
       10000
     );
-    this.instance.position.set(46, 34, 38);
+
+    // place the camera on the character
+    this.instance.position.y = 2;
+    this.instance.position.z = 5;
+    this.instance.position.x = 0;
+
     scene.add(this.instance);
   }
 
