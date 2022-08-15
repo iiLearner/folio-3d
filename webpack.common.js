9911d6a8005b674fe7206f0d49cbff611263cc8f
iLearner <me@ilearner.dev>
2022-08-15 11:32:10 -0400

    feat: webpack changes

--- webpack.common.js
+++ webpack.common.js
@@ -71,6 +71,12 @@ module.exports = {
           },
         ],
       },
+      // Shaders
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader", "glslify-loader"],
+      },
     ],
   },
 
